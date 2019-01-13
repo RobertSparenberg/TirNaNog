7f3a43f4fefe934a87adb613db0dbc43a18f8f12
imahilus <robertsparenberg@frozenchaos.net>
2019-01-13 23:59:13 +0000

    application.properties is now consulted for hibernate settings. orm.xml still has mistakes.

--- control/src/main/java/net/frozenchaos/TirNaNog/AppConfig.java
+++ control/src/main/java/net/frozenchaos/TirNaNog/AppConfig.java
@@ -1,23 +1,26 @@
 package net.frozenchaos.TirNaNog;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
 public class AppConfig {
     private final Properties properties;
 
-    @Autowired
-    public AppConfig(Properties properties) {
-        this.properties = properties;
+    public AppConfig() throws IOException {
+        this.properties = new Properties();
+        properties.load(new FileInputStream("application.properties"));
+        int i = 0;
     }
 
     @Bean
@@ -39,6 +42,7 @@ public class AppConfig {
         factory.setPersistenceXmlLocation("persistence.xml");
         factory.setPersistenceUnitName("TirNaNog");
         factory.setJpaProperties(properties);
+        factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
         return factory;
     }
